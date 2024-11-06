6aa4a2e867a8e03af30035965305ff9aa4b57718
sajjad <sajjad.sisakhtpour@gmail.com>
2024-11-07 03:26:32 +0330

    add clear method for clearRect animate

--- game.js
+++ game.js
@@ -1,6 +1,13 @@
 /////////////////canvas////////////////////
 
-function Canvas2D() {}
+function Canvas2D() {
+  this._canvas = document.getElementById("screen");
+  this.ctx = this._canvas.getContext("2d");
+}
+
+Canvas2D.prototype.clear = function () {
+  this.ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+};
 
 let canvas = new Canvas2D();
 
