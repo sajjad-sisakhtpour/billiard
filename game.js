d2ad80dfea6202dda723a70119a81f486439a539
sajjad <sajjad.sisakhtpour@gmail.com>
2024-11-07 03:31:19 +0330

    add draw image method in canvas class

--- game.js
+++ game.js
@@ -9,6 +9,21 @@ Canvas2D.prototype.clear = function () {
   this.ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
 };
 
+Canvas2D.prototype.drawImage = function (
+  image,
+  x = 0,
+  y = 0,
+  dx = 0,
+  dy = 0,
+  rotation = 0
+) {
+  this.ctx.save();
+  this.ctx.translate(x, y);
+  this.ctx.rotate(rotation);
+  this.ctx.drawImage(image, dx, dy);
+  this.ctx.restore();
+};
+
 let canvas = new Canvas2D();
 
 //////////////////////////////////////
